3a0371dfe4483c92e7c26f73d079a135411b7585
Homie-Khanh <156377690+Homie-Khanh@users.noreply.github.com>
2024-04-23 17:52:06 -0400

    add item user id update

--- src/Main/Create/index.tsx
+++ src/Main/Create/index.tsx
@@ -8,7 +8,8 @@ import { useState } from "react";
 
 function AddItem() {
   const [currentUserId, setCurrentUserId] = useState(
-    "662715966380bcfe90f7ff62"
+    // "662715966380bcfe90f7ff62"
+    "6627c1ea94b3a14d5416ae18"
   );
 
   const [post, setPost] = useState<Post>({
